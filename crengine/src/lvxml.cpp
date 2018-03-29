dcdfecab77198d53179fb21317dad87361aeefd1
Tarasus <ya_bober@mail.ru>
2018-03-29 12:46:09 +0300

    Parse() method overloaded call fix, "&shy;" tag sanitizing fix for hyphenation.
    Signed-off-by: Tarasus <ya_bober@mail.ru>

--- crengine/src/lvxml.cpp
+++ crengine/src/lvxml.cpp
@@ -2606,228 +2606,7 @@ bool LvXmlParser::CheckFormat() {
 }
 bool LvXmlParser::Parse()
 {
-	Reset();
-	callback_->OnStart(this);
-	//int txt_count = 0;
-	int flags = callback_->getFlags();
-	bool error = false;
-	bool in_xml_tag = false;
-	bool close_flag = false;
-	bool q_flag = false;
-	bool body_started = false;
-	bool firstpage_thumb_num_reached = false;
-	int fragments_counter = 0;
-	lString16 tagname;
-	lString16 tagns;
-	lString16 attrname;
-	lString16 attrns;
-	lString16 attrvalue;
-
-	for (; !eof_ && !error && !firstpage_thumb_num_reached;)
-	{
-		if (m_stopped)
-			break;
-		// Load next portion of data if necessary
-		lChar16 ch = PeekCharFromBuffer();
-		switch (m_state)
-		{
-			case ps_bof:
-			{
-				//CRLog::trace("LvXmlParser::Parse() ps_bof");
-				// Skip file beginning until '<'
-				for ( ; !eof_ && ch!='<'; ch = PeekNextCharFromBuffer())
-					;
-				if (!eof_)
-				{
-					m_state = ps_lt;
-					ReadCharFromBuffer();
-				}
-				//CRLog::trace("LvXmlParser::Parse() ps_bof ret");
-			}
-				break;
-			case ps_lt:
-			{
-				//CRLog::trace("LvXmlParser::Parse() ps_lt");
-				if (!SkipSpaces())
-					break;
-				close_flag = false;
-				q_flag = false;
-				if (ch=='/')
-				{
-					ch = ReadCharFromBuffer();
-					close_flag = true;
-				}
-				else if (ch=='?')
-				{
-					// <?xml?>
-					ch = ReadCharFromBuffer();
-					q_flag = true;
-				}
-				else if (ch=='!')
-				{
-					// comments etc...
-					if (PeekCharFromBuffer(1) == '-' && PeekCharFromBuffer(2) == '-') {
-						// skip comments
-						ch = PeekNextCharFromBuffer(2);
-						while (!eof_ && (ch != '-' || PeekCharFromBuffer(1) != '-'
-						                 || PeekCharFromBuffer(2) != '>') ) {
-							ch = PeekNextCharFromBuffer();
-						}
-						if (ch=='-' && PeekCharFromBuffer(1)=='-'
-						    && PeekCharFromBuffer(2)=='>' )
-							ch = PeekNextCharFromBuffer(2);
-						m_state = ps_text;
-						break;
-					}
-				}
-				if (!ReadIdent(tagns, tagname) || PeekCharFromBuffer()=='=')
-				{
-					// Error
-					if (SkipTillChar('>'))
-					{
-						m_state = ps_text;
-						ch = ReadCharFromBuffer();
-					}
-					break;
-				}
-				if (possible_capitalized_tags_) {
-					tagns.lowercase();
-					tagname.lowercase();
-				}
-				if (close_flag)
-				{
-					callback_->OnTagClose(tagns.c_str(), tagname.c_str());
-					//CRLog::trace("</%s>", LCSTR(tagname));
-					if (SkipTillChar('>'))
-					{
-						m_state = ps_text;
-						ch = ReadCharFromBuffer();
-					}
-					break;
-				}
-
-				if (q_flag) {
-					tagname.insert(0, 1, '?');
-					in_xml_tag = (tagname == "?xml");
-				} else {
-					in_xml_tag = false;
-				}
-				callback_->OnTagOpen(tagns.c_str(), tagname.c_str());
-				//CRLog::trace("OnTagOpen %s", LCSTR(tagname));
-				if (!body_started && tagname == "body")
-					body_started = true;
-				m_state = ps_attr;
-				//CRLog::trace("LvXmlParser::Parse() ps_lt ret");
-			}
-				break;
-			case ps_attr:
-			{
-				//CRLog::trace("LvXmlParser::Parse() ps_attr");
-				if (!SkipSpaces())
-					break;
-				ch = PeekCharFromBuffer();
-				lChar16 nch = PeekCharFromBuffer(1);
-				if (ch == '>' || (nch == '>' && (ch == '/' || ch == '?')))
-				{
-					callback_->OnTagBody();
-					// end of tag
-					if (ch != '>')
-						callback_->OnTagClose(tagns.c_str(), tagname.c_str());
-					if (ch == '>')
-						ch = PeekNextCharFromBuffer();
-					else
-						ch = PeekNextCharFromBuffer(1);
-					m_state = ps_text;
-					break;
-				}
-				if (!ReadIdent(attrns, attrname))
-				{
-					// error: skip rest of tag
-					SkipTillChar('<');
-					ch = PeekNextCharFromBuffer(1);
-					callback_->OnTagBody();
-					m_state = ps_lt;
-					break;
-				}
-				SkipSpaces();
-				attrvalue.reset(16);
-				ch = PeekCharFromBuffer();
-				// Read attribute value
-				if (ch == '=')
-				{
-					// Skip '='
-					ReadCharFromBuffer();
-					SkipSpaces();
-					lChar16 qChar = 0;
-					ch = PeekCharFromBuffer();
-					if (ch == '\"' || ch == '\'')
-					{
-						qChar = ReadCharFromBuffer();
-					}
-					for (; !eof_;)
-					{
-						ch = PeekCharFromBuffer();
-						if (ch == '>')
-							break;
-						if (!qChar && IsSpaceChar(ch))
-							break;
-						if (qChar && ch == qChar)
-						{
-							ch = PeekNextCharFromBuffer();
-							break;
-						}
-						ch = ReadCharFromBuffer();
-						if (ch)
-							attrvalue += ch;
-						else
-							break;
-					}
-				}
-				if (possible_capitalized_tags_) {
-					attrns.lowercase();
-					attrname.lowercase();
-				}
-				if ((flags & TXTFLG_CONVERT_8BIT_ENTITY_ENCODING) && m_conv_table) {
-					PreProcessXmlString(attrvalue, 0, m_conv_table);
-				}
-				callback_->OnAttribute(attrns.c_str(), attrname.c_str(), attrvalue.c_str());
-				if (in_xml_tag && attrname == "encoding")
-				{
-					SetCharset(attrvalue.c_str());
-				}
-				//CRLog::trace("LvXmlParser::Parse() ps_attr ret");
-			}
-				break;
-			case ps_text:
-			{
-				//CRLog::trace("LvXmlParser::Parse() ps_text");
-//                if ( dumpActive ) {
-//                    lString16 s;
-//                    s << PeekCharFromBuffer(0) << PeekCharFromBuffer(1) << PeekCharFromBuffer(2) << PeekCharFromBuffer(3)
-//                      << PeekCharFromBuffer(4) << PeekCharFromBuffer(5) << PeekCharFromBuffer(6) << PeekCharFromBuffer(7);
-//                    CRLog::trace("text: %s...", LCSTR(s) );
-//                    dumpActive = true;
-//                }
-//                txt_count++;
-//                if ( txt_count<121 ) {
-//                    if ( txt_count>118 ) {
-//                        CRLog::trace("Text[%d]:", txt_count);
-//                    }
-//                }
-				ReadText();
-				fragments_counter++;
-
-				m_state = ps_lt;
-				//CRLog::trace("LvXmlParser::Parse() ps_text ret");
-			}
-				break;
-			default:
-			{
-			}
-		}
-	}
-	callback_->OnStop();
-	return !error;
+    return Parse(false);
 }
 
 bool LvXmlParser::Parse(bool need_coverpage)
@@ -3043,9 +2822,8 @@ bool LvXmlParser::Parse(bool need_coverpage)
             ReadText();
             fragments_counter++;
 
-	        //if(need_coverpage)
-			if(0)
-	        {
+	        if(need_coverpage)
+			{
                 CRLog::trace("LvXmlParser: text fragments read : %d", fragments_counter);
 				if (fragments_counter >= FIRSTPAGE_BLOCKS_MAX)
 				{
@@ -3403,7 +3181,14 @@ int PreProcessXmlString(lChar16* str, int len, lUInt32 flags, const lChar16* enc
                     state = 0;
                     if ( enc_table && code<256 && code>=128 )
                         code = enc_table[code - 128];
-                    str[j++] = code;
+                    if (code == 173) // 173 == &shy;
+                    {
+                        //do nothing. that removes shy tags from output
+                    }
+                    else
+                    {
+                        str[j++] = code;
+                    }
                     nsp = 0;
                 } else {
                     // include & and rest of entity into output string
