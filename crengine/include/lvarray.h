e49a7cf7c18ffa6aaa909aacfe2f04c42a29532f
Dmitry Razdrokin <d.razdrokin@gmail.com>
2018-04-12 13:26:56 +0300

    Removed unused reference management routines

--- crengine/include/lvarray.h
+++ crengine/include/lvarray.h
@@ -231,68 +231,6 @@ public:
     ~LVArray() { clear(); }
 };
 
-template <typename T >
-class LVArrayQueue
-{
-private:
-    LVArray<T> m_buf;
-    int inpos;
-public:
-    LVArrayQueue()
-        : inpos(0)
-    {
-    }
-
-    /// returns pointer to reserved space of specified size
-    T * prepareWrite( int size )
-    {
-        if ( m_buf.length() + size > m_buf.size() )
-        {
-            if ( inpos > (m_buf.length() + size) / 2 )
-            {
-                // trim
-                m_buf.erase(0, inpos);
-                inpos = 0;
-            }
-        }
-        return m_buf.addSpace( size );
-    }
-
-    /// writes data to end of queue
-    void write( const T * data, int size )
-    {
-        T * buf = prepareWrite( size );
-        for (int i=0; i<size; i++)
-            buf[i] = data[i];
-    }
-
-    int length()
-    {
-        return m_buf.length() - inpos;
-    }
-
-    /// returns pointer to data to be read
-    T * peek() { return m_buf.ptr() + inpos; }
-
-    /// reads data from start of queue
-    void read( T * data, int size )
-    {
-        if ( size > length() )
-            size = length();
-        for ( int i=0; i<size; i++ )
-            data[i] = m_buf[inpos + i];
-        inpos += size;
-    }
-
-    /// skips data from start of queue
-    void skip( int size )
-    {
-        if ( size > length() )
-            size = length();
-        inpos += size;
-    }
-};
-
 typedef LVArray<lUInt8> LVByteArray;
 typedef LVRef<LVByteArray> LVByteArrayRef;
 
