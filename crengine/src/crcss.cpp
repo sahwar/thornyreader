08e0eb2c6772023a8b5b4810d12412944b1c9489
Tarasus <ya_bober@mail.ru>
2018-04-30 16:11:15 +0300

    [FIX] Fix for monospace code rendering
    Signed-off-by: Tarasus <ya_bober@mail.ru>

--- crengine/src/crcss.cpp
+++ crengine/src/crcss.cpp
@@ -161,7 +161,7 @@ h4 > p, h5 > p, h6 > p, subtitle > p,
 }
 
 pre, code, .code {
-  display: block;
+  display: inline;
   white-space: pre;
   text-align: left;
   text-indent: 0em;
@@ -169,11 +169,11 @@ pre, code, .code {
   margin-bottom: 0em;
   margin-left: 0em;
   margin-right: 0em;
-  font-family: "Droid Sans Mono", monospace;
+  font-family: "Roboto Mono", monospace;
 }
 tt, samp, kbd {
   display: inline;
-  font-family: "Droid Sans Mono", monospace;
+  font-family: "Roboto Mono", monospace;
 }
 
 blockquote {
@@ -200,7 +200,7 @@ ol, ul {
   margin-bottom: 1em;
   margin-left: 0em;
   margin-right: 0em;
-  padding-left: 40px;
+  padding-left: 20px;
 }
 ol {
   list-style-type: decimal;
