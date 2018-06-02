32735203b1dfb560c0aa1af4a2bcc1fcae8d35c9
Dmitry Razdrokin <d.razdrokin@gmail.com>
2018-06-02 06:26:44 +0300

    debug_generate_long_backtrace

--- thornyreader/include/debug_generate_crash.h
+++ thornyreader/include/debug_generate_crash.h
@@ -3,5 +3,8 @@
 
 void debug_generate_sigsegv_segv_maperr();
 void debug_generate_busyloop();
+void debug_generate_long_backtrace();
+void debug_proxy_call_complex_two();
+void debug_proxy_call_oneline();
 
 #endif //_DEBUG_GENERATE_CRASH_H
--- thornyreader/src/debug_generate_crash.cpp
+++ thornyreader/src/debug_generate_crash.cpp
@@ -1,6 +1,7 @@
 
 #include "debug_generate_crash.h"
 #include <unistd.h>
+#include <cstdio>
 
 static volatile bool true_var = true;
 
@@ -17,5 +18,35 @@ void debug_generate_busyloop()
     while (true_var) {
         sleep(1);
     }
+#endif
+}
+
+void debug_proxy_call_complex_two()
+{
+#ifdef TRDEBUG
+    printf("debug_proxy_call_complex_two");
+    debug_generate_sigsegv_segv_maperr();
+#endif
+}
+
+static void debug_proxy_call_oneline_static()
+{
+#ifdef TRDEBUG
+    debug_proxy_call_complex_two();
+#endif
+}
+
+void debug_proxy_call_oneline()
+{
+#ifdef TRDEBUG
+    debug_proxy_call_oneline_static();
+#endif
+}
+
+void debug_generate_long_backtrace()
+{
+#ifdef TRDEBUG
+    printf("debug_generate_long_backtrace");
+    debug_proxy_call_oneline();
 #endif
 }
